6407098cfcc06c146d4dca645fc3f70fe99d1a51
Krishan Kant <34978606+krishan226@users.noreply.github.com>
2025-07-02 11:54:51 +0530

    Swift UI default project added

---
+++ SwiftUI_Practice/SwiftUI_PracticeTests/SwiftUI_PracticeTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PracticeTests.swift
+//  SwiftUI_PracticeTests
+//
+//  Created by Krishan on 7/2/25.
+//
+
+import Testing
+@testable import SwiftUI_Practice
+
+struct SwiftUI_PracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
